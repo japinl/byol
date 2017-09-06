f24ccbb72b963cc818d95451602e9fd3d6e4bd9e
japinl <japinli@hotmail.com>
2017-09-06 22:35:45 +0800

    Add error handling

--- parsing.c
+++ parsing.c
@@ -22,33 +22,127 @@ void add_history(char *unused)
 #endif
 
 /*
- * @brief Use operator string to see which operation to preform.
+ * Create enumeration of possible error types.
  */
-long eval_op(long x, char *op, long y)
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/*
+ * Create enumeration of possible lval types.
+ */
+enum { LVAL_NUM, LVAL_ERR };
+
+/*
+ * Declare new lval structure.
+ */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/*
+ * Create a new number type lval.
+ */
+lval lval_num(long x)
 {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    if (strcmp(op, "%") == 0) { return x % y; }
-    if (strcmp(op, "^") == 0) { return x << y; }
-    if (strcmp(op, "min") == 0) { return x < y ? x : y; }
-    if (strcmp(op, "max") == 0) { return x < y ? y : x; }
-    return 0;
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t *t)
+/*
+ * Create a new error type lval.
+ */
+lval lval_err(int x)
+{
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+/*
+ * Print an "lval".
+ */
+void lval_print(lval v)
+{
+    switch (v.type) {
+        /*
+         * In the case the type is a number print it.
+         * Then 'break' out of the switch.
+         */
+    case LVAL_NUM:
+        printf("%li", v.num);
+        break;
+
+        /*
+         * In the case the type is an error.
+         */
+    case LVAL_ERR:
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division by zero!");
+        }
+        if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid operator!");
+        }
+        if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid number!");
+        }
+        break;
+    }
+}
+
+/*
+ * Print an "lval" followed by a newline.
+ */
+void lval_println(lval v)
+{
+    lval_print(v);
+    putchar('\n');
+}
+
+
+/*
+ * @brief Use operator string to see which operation to preform.
+ */
+lval eval_op(lval x, char *op, lval y)
+{
+    /* If either value is an error return it. */
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    /* Otherwise do maths on the number values */
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    }
+    if (strcmp(op, "%") == 0) {
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+    }
+    if (strcmp(op, "^") == 0) { return lval_num(x.num << y.num); }
+    if (strcmp(op, "min") == 0) { return lval_num(x.num < y.num ? x.num : y.num); }
+    if (strcmp(op, "max") == 0) { return lval_num(x.num < y.num ? y.num : x.num); }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t *t)
 {
     /* If tagged as number return directly. */
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     /* The operator is always stored in second child. */
     char *op = t->children[1]->contents;
 
     /* Store the third child in `x'. */
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     /* Iterate the remaining children and combining. */
     int i = 3;
@@ -88,8 +182,8 @@ int main(int argc, char **argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, byol, &r)) {
             /* On success print and delete the AST */
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise, print and delete the error */
